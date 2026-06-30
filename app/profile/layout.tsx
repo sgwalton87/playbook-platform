9c6c97311486694e7e30386ae527dea6d624895c
Stephisha Walton <you@youremail.com>
2026-06-30 14:26:02 -0700

    Restore all layout.tsx files with AppShell

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,4 +1,4 @@
 import AppShell from "@/components/AppShell";
-export default function SectionLayout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return <AppShell>{children}</AppShell>;
 }
