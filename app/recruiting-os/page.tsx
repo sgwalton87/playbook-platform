c51b34ca1ea65c91b9a61783b55c3403764810a9
sgwalton87 <stephishawalton@gmail.com>
2026-08-16 01:17:10 -0700

    feat: activate verified Recruiting OS after scope approval

--- app/recruiting-os/page.tsx
+++ app/recruiting-os/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import RecruitingVerificationExperience from "@/components/recruiting/RecruitingVerificationExperience";
+import VerifiedRoleActivationBridge from "@/components/role-os/VerifiedRoleActivationBridge";
 
 export default function RecruitingOSPage() {
-  return <RecruitingVerificationExperience />;
+  return (
+    <VerifiedRoleActivationBridge roleOS="recruiter" verificationEndpoint="/api/recruiting-verification">
+      <RecruitingVerificationExperience />
+    </VerifiedRoleActivationBridge>
+  );
 }
