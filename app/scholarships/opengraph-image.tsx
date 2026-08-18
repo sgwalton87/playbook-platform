48de0b34590f700ef6a53e0874e2eee616c7119e
sgwalton87 <stephishawalton@gmail.com>
2026-08-18 13:50:49 -0700

    Phase 9: omit generated Scholarship social image

---
+++ app/scholarships/opengraph-image.tsx
@@ -0,0 +1,4 @@
+export const alt="Playbook Scholarship Finder";
+export const size={width:1200,height:630};
+export const contentType="image/png";
+export default function Image(){return <div style={{width:"100%",height:"100%",display:"flex",alignItems:"center",justifyContent:"center",background:"#081D34",color:"white",fontSize:72,fontWeight:900}}>Playbook Scholarship Finder</div>;}
